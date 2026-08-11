8134a33ec6006267804d6dc70eb4429b8be1d5dd
Roosevelt Wooden <roosevelt.wooden@students.maestrocollege.edu>
2026-08-11 09:09:02 -0400

    Build Lumi from source template

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,17 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/lumi/",
+  build: {
+    rollupOptions: {
+      input: "source.html",
+      output: {
+        entryFileNames: "assets/lumi-app.js",
+        chunkFileNames: "assets/[name]-[hash].js",
+        assetFileNames: (assetInfo) =>
+          assetInfo.names?.some((name) => /\.css$/.test(name))
+            ? "assets/lumi-app.css"
+            : "assets/[name]-[hash][extname]",
+      },
+    },
+  },
 });
